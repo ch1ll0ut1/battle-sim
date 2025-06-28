6c0cb2673b46231ba419e55f6eb0585856878ec8
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2025-06-29 02:26:42 +0700

    Update jest config to handle ESM

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
@@ -15,4 +15,13 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'json', 'html'],
   moduleFileExtensions: ['ts', 'js', 'json'],
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapping: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 } 
