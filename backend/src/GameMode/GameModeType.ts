401a4875ce38c98913b960007965610d00a5d874
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2025-07-16 12:55:05 +0700

    Change game mode selection to use constructor instead of config string

--- backend/src/GameMode/GameModeType.ts
+++
@@ -1,6 +0,0 @@
-import { GameModeConstructor } from './GameMode';
-import { MovementSandbox } from './MovementSandbox/MovementSandbox';
-
-export const GAME_MODE_TYPE = {
-    movementSandbox: MovementSandbox,
-} as const satisfies Record<string, GameModeConstructor>;
