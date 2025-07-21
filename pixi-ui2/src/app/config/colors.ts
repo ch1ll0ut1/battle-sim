af17c8ecf275cfe8d4d7efeec85f83a991e3abc5
Ch1ll0ut1 <6835266+ch1ll0ut1@users.noreply.github.com>
2025-07-21 16:18:14 +0700

    Add configs for colors and font

---
+++ pixi-ui2/src/app/config/colors.ts
@@ -0,0 +1,16 @@
+const colors = {
+    primary: '#e72264',
+    secondary: '',
+    text: '#ffffff',
+    background: '#1099bb',
+    button: {
+        background: '#3d3d3d',
+        text: '#ffffff',
+    },
+    progressBar: {
+        backgroundColor: '#3d3d3d',
+        fillColor: '#e72264',
+    },
+};
+
+export default colors;
---
+++ pixi-ui2/src/app/config/font.ts
@@ -0,0 +1,21 @@
+import colors from './colors';
+
+const font = {
+    title: {
+        fontFamily: 'Verdana',
+        fontSize: 24,
+        fill: colors.primary,
+    },
+    subTitle: {
+        fontFamily: 'Verdana',
+        fontSize: 20,
+        fill: colors.secondary,
+    },
+    text: {
+        fontFamily: 'Verdana',
+        fontSize: 16,
+        fill: colors.text,
+    },
+};
+
+export default font;
